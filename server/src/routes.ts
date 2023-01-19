9969374eeafa2a35f82ec286b60895d98bc9263d
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2023-01-19 13:00:23 -0300

    added aula2 - backend

---
+++ server/src/routes.ts
@@ -0,0 +1,76 @@
+
+import { FastifyInstance } from "fastify";
+import { prisma } from "./lib/prisma";
+import {z} from 'zod';
+import dayjs from 'dayjs';
+
+export async function appRoutes(app: FastifyInstance){
+    app.post('/habits', async (request) => {
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number().min(0).max(6)
+                )
+        })
+        const {title, weekDays} = createHabitBody.parse(request.body);
+
+        const today = dayjs().startOf('day').toDate();
+        await prisma.habit.create({
+            data: {
+                title,
+                created_at: new Date(),
+                weekDays: {
+                    create: weekDays.map((weekDay) =>{
+                        return {
+                            week_day:weekDay
+                        }
+                    })
+                }
+            }
+        })
+    });
+    
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            //converte a data em instância de Date
+            date: z.coerce.date()
+        }) 
+        const {date} = getDayParams.parse(request.query);
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day');
+        //todos os hábitos possiveis
+        //habitos que ja foram completados
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at:{
+                    lte: date
+                },
+                weekDays:{
+                    some:{
+                        week_day: weekDay
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where:{
+                date: parsedDate.toDate()
+            },
+            include:{
+                dayHabits: true
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map((dayHabit) => 
+        dayHabit.habit_id)
+        
+        return {
+            possibleHabits,
+            completedHabits
+        }
+    })
+}
+
