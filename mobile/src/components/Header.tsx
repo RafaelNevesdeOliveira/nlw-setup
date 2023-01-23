b4c16361648fc0307b7d496b8e0d5acc545dde70
rafael.noliveira <rafael.noliveira@totvs.com.br>
2023-01-23 18:15:03 -0300

    aula2 - mobile

---
+++ mobile/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import Logo from '../assets/logo.svg';
+export function Header(){
+    return (
+        <View className="w-full flex-row items-center justify-between">
+            <Logo/>
+        </View>
+    )
+}
