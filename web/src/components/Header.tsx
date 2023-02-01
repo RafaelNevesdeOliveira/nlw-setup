69a25b058e19a2e8f219c4f0aedb2b5dc179ed5a
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2023-02-01 00:52:37 -0300

    front end -aula3

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,16 +1,37 @@
+import { useState } from "react";
+import logo from "../assets/logo.svg";
+import { Plus, X } from "phosphor-react";
+import * as Dialog from "@radix-ui/react-dialog";
 
-import logo from '../assets/logo.svg'
-import {Plus} from 'phosphor-react'
+export function Header() {
+  return (
+    <div className="w-full max-w-3x1 mx-auto flex items-center justify-between">
+      <img src={logo} alt="Habits" />
 
-export function Header(){
-    return(
-        <div className='w-full max-w-3x1 mx-auto flex items-center justify-between'>
-          <img src={logo} alt="Habits" />
-          <button 
-          type='button' 
-          className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'>
-            <Plus size={20} className='text-violet-500' />
-            Novo hábito</button>
-        </div>
-    )
+      <Dialog.Root>
+        <Dialog.Trigger>
+          <button
+            type="button"
+            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
+          >
+            <Plus size={20} className="text-violet-500" />
+            Novo hábito
+          </button>
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          {/* Overlay --> aquele overlay que fica no fundo do modal */}
+          <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+          <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
+              <X size={24} aria-label="Fechar"></X>
+            </Dialog.Close>
+            <Dialog.Title className="text-3x1 leading-tight font-extrabold">
+              Criar Hábito
+            </Dialog.Title>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </div>
+  );
 }
---
+++ web/src/components/NewHabitForm.tsx
@@ -0,0 +1,7 @@
+export function NewHabitForm(){
+    return (
+        <form>
+            
+        </form>
+    )
+}
