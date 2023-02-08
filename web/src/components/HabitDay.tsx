83cdef251a59e13fc40f4d5a076c4e96d90e6f5b
RafaelNevesdeOliveira <rafaelneves652@gmail.com>
2023-02-07 23:34:02 -0300

    aula 3 - frontend

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,16 +1,42 @@
 import * as Popover from "@radix-ui/react-popover";
-import { ProgressBar } from './ProgressBar';
+import { ProgressBar } from "./ProgressBar";
+import clsx from "clsx";
 
-export function HabitDay() {
+interface HabitDayProps {
+  completed: number;
+  amount: number;
+}
+
+export function HabitDay(props: HabitDayProps) {
+  const amountCompletedPercentage = Math.round(
+    (props.completed / props.amount) * 100
+  );
+
   return (
     <Popover.Root>
-      <Popover.Trigger className="w-10 h-10 bg-zinc-900 bottom-2 border-zinc-800 rounded-lg">
+      <Popover.Trigger
+        className={clsx(
+          "w-10 h-10 border-2 rounded-lg",
+          {
+            'bg-violet-500 border-violet-200': amountCompletedPercentage >= 80,
+            'bg-violet-600 border-violet-300':
+              amountCompletedPercentage >= 60 && amountCompletedPercentage < 80,
+            'bg-violet-700 border-violet-400':
+              amountCompletedPercentage >= 40 && amountCompletedPercentage < 60,
+            'bg-violet-800 border-violet-500':
+              amountCompletedPercentage >= 20 && amountCompletedPercentage < 40,
+            'bg-violet-900 border-violet-600':
+              amountCompletedPercentage > 0 && amountCompletedPercentage < 20,
+            'bg-zinc-900 border-zinc-800': amountCompletedPercentage === 0,
+          }
+        )}
+      >
         <Popover.Content className="bg-zinc-900 border-zinc-800 rounded-2xl min-w-[320px] p-6 flex flex-col items-start">
           <span className="font-semibold text-zinc-400">terça-feira</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">
             02/01
           </span>
-          <ProgressBar progress={50}/>
+          <ProgressBar progress={50} />
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
       </Popover.Trigger>
