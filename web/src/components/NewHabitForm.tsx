ace31f9db1b4ea4143cd77ab2da1d4f57c955d33
rafael.noliveira <rafael.noliveira@totvs.com.br>
2023-07-15 13:14:44 -0300

    aula 4

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -14,11 +14,22 @@ const availableWeekDays = [
 
 export function NewHabitForm() {
   const [title, setTitle] = useState("");
-  const [weekDays, setWeekDays] = useState([]);
+  const [weekDays, setWeekDays] = useState<number[]>([]);
 
   function createNewHabit(event: FormEvent) {
     event.preventDefault();
-    console.log("event", event, title);
+  }
+
+  function handleToggleWeekDay(weekDay: number){
+    if(weekDays.includes(weekDay)){
+      const weekDaysWithRemovedOne = weekDays.filter(day => day === weekDay)
+
+      setWeekDays(weekDaysWithRemovedOne)
+    }else{
+      const weekDaysWithAddedOne = [...weekDays, weekDay]
+
+      setWeekDays(weekDaysWithAddedOne)
+    }
   }
 
   return (
@@ -39,12 +50,14 @@ export function NewHabitForm() {
       </label>
 
       <div className="flex flex-col gap-2 mt-3">
-        {availableWeekDays.map((weekDay) => {
+        {availableWeekDays.map((weekDay, index) => {
           return (
             <Checkbox.Root
               key={weekDay}
               className="flex items-center gap-3 group"
-              onCheckedChange={weekDays}
+              onCheckedChange={() => {
+                handleToggleWeekDay(index)
+              }}
             >
               <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50">
                 <Checkbox.Indicator>
